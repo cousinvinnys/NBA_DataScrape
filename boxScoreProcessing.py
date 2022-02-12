2c01f853de46aaedd56aa85ffe0982723238121d
Christian Peters <christianpeters31@outlook.com>
2022-02-12 01:20:20 -0500

    Got 'did team play yesterday' portion working! All you need is an abbreviation. Returns the box score of the game if the team played yesterday.

--- boxScoreProcessing.py
+++ boxScoreProcessing.py
@@ -12,110 +12,61 @@ import numpy as np
 nba_teams = teams.get_teams()
 
 
+# Returns the ID of the team designated by the abbreviation
 def get_team_id_abbrev(team_name):
     temp_team = [team for team in nba_teams if team['abbreviation'] == team_name][0]
     return temp_team['id']
 
 
-def did_play_yesterday(teamID):
-
+# Returns DataFrame of latest game
+def get_latest_game(teamID):
     teamGameLog = teamgamelog.TeamGameLog(team_id=teamID)
-    gameListDF = teamGameLog.get_data_frames()[0].iloc[0]['GAME_DATE']
-    formatted_date = pd.to_datetime(gameListDF)
-
-    if(formatted_date == date.today() - timedelta(days=1)):
-        print("yes, your team played yesterday!")
-        return True
-    else:
-        print("no, your team did not play yesterday")
-        return False
+    return teamGameLog.get_data_frames()[0].iloc[0]
 
 
-did_play_yesterday(get_team_id_abbrev('IND'))
+# Boolean to check if the team played a game yesterday
+def did_play_yesterday(teamID):
+    teamGameLog = teamgamelog.TeamGameLog(team_id=teamID)
 
-cavsID = get_team_id_abbrev('MIA')
-# print(cavsID)
+    # Get the latest game date by the latest game DataFrame
+    latest_game_date = get_latest_game(teamID)['GAME_DATE']
 
-# Cavs gamelog from this season
-cavsGameLog = teamgamelog.TeamGameLog(team_id=cavsID)
+    # Format the date to be compatible with the API using pandas (!!!)
+    formatted_date = pd.to_datetime(latest_game_date)
 
-# Get the latest game
-cavsGameLog_latest = cavsGameLog.get_data_frames()
-# print(cavsGameLog_latest)
+    # Sourcery being a GOAT (!!!)
+    return (formatted_date == date.today() - timedelta(days=1))
 
-# print(cavsGameLog_latest[0].iloc[0])
-
-new_df = cavsGameLog_latest[0].iloc[0]
-# print(new_df)
-
-# print(new_df['GAME_DATE'])
-
-# Save the game date
-game_date = new_df['GAME_DATE']
-
-# Compare the game date to the current date
-
-# print(game_date)
-# print(dt.datetime.now())
-formatted_date = pd.to_datetime(game_date)
-# print(formatted_date)
-
-# Check if formatted_date is equal to yesterday's date
-# if(formatted_date == date.today() - timedelta(days=1)):
-#print("yes, your team played yesterday!")
-# else:
-#print("no, your team did not play yesterday")
-
-yesterdays_date = date.today() - timedelta(1)
+# Need to work on an interface, or if I'm just going to pass the abbreviation through the command line
 
 
-# creating the date object of today's date
-todays_date = date.today()
-#print("Today's Date: ", todays_date)
+# Gets team data for the frame
+teamID = get_team_id_abbrev('PHI')
+teamGameLog = teamgamelog.TeamGameLog(team_id=teamID)
+latest_game = get_latest_game(teamID)
+latest_game_ID = latest_game['Game_ID']
 
-#print("Yesterday's date: ", yesterdays_date)
+# If the team played yesterday, get the box score
+if did_play_yesterday(teamID):
+    testBoxScore = box_score.BoxScoreTraditionalV2(game_id=latest_game_ID)
+else:
+    print('No game yesterday')
+    exit()
 
-# Check if a game was played yesterday
+# Grabbing the first dataframe from the get_data_frames() function, which is the box score
+df = testBoxScore.get_data_frames()[0]
 
+# Dropping NaN values (Coach's decisions to not play)
+df = df.dropna()
 
-# importing date class from datetime module
-
-
-# printing todays date
-#print("Current date: ", todays_date)
-
-# fetching the current year, month and day of today
-#print("Current year:", todays_date.year)
-#print("Current month:", todays_date.month)
-#print("Current day:", todays_date.day)
-
-
-testBoxScore = box_score.BoxScoreTraditionalV2(game_id='0022100825')
-df = testBoxScore.get_data_frames()[1]
-
-# print(df)
-
-"""
+# Dropping columns that aren't needed
 dropped_columns = ['COMMENT', 'MIN', 'NICKNAME', 'TEAM_CITY', 'START_POSITION']
-
 for column in dropped_columns:
     df = df.drop([column], axis='columns')
 
-dropped_rows = [12]
-
-for row in dropped_rows:
-    df = df.drop([row], axis='rows')
-
-
-# df = df.astype({"PTS": 'int', ""})
-
+# Converting the floats to ints where they are needed
 cleaned_columns = ['PTS', 'PLUS_MINUS', 'PF', 'TO', 'BLK', 'STL', 'AST', 'REB', 'DREB', 'OREB', 'FTA', 'FTM', 'FG3A', 'FG3M', 'FGA', 'FGM']
-
 for column in cleaned_columns:
     df[[column]] = df[[column]].apply(np.int64)
 
-
-# print(df)
-# print(df.columns)
-
-"""
+print(df)
