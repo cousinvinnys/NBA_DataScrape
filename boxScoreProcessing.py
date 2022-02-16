4621c42f343e763741cfbe63d18c738b5467cf92
Christian Peters <christianpeters31@outlook.com>
2022-02-16 16:57:55 -0500

    Minor refactor, added main function and more organization of the code. Next up is to clean and organize the team data, and start to format for email!

--- boxScoreProcessing.py
+++ boxScoreProcessing.py
@@ -1,24 +1,27 @@
 from datetime import date, timedelta, datetime
 from hashlib import new
 from matplotlib.pyplot import get
-from nba_api.stats.endpoints import boxscoretraditionalv2 as box_score
-from nba_api.stats.endpoints import teamgamelog
-from nba_api.stats.static import teams
-from nba_api.stats.static import players
+from nba_api.stats.endpoints import teamgamelog, boxscoretraditionalv2 as box_score
+from nba_api.stats.static import teams, players
 
 import pandas as pd
 import numpy as np
 
-# Returns the ID of the team designated by the abbreviation
+
 def get_team_id_abbrev(team_name):
+    '''
+    Returns the ID of the team designated by the abbreviation
+    '''
     nba_teams = teams.get_teams()
     temp_team = [team for team in nba_teams if team['abbreviation'] == team_name][0]
     return temp_team['id']
+
 
 # Returns DataFrame of latest game
 def get_latest_game(teamID):
     teamGameLog = teamgamelog.TeamGameLog(team_id=teamID)
     return teamGameLog.get_data_frames()[0].iloc[0]
+
 
 # Boolean to check if the team played a game provided by the day offset
 def did_play_yesterday(teamID):
@@ -33,53 +36,9 @@ def did_play_yesterday(teamID):
     # Sourcery being a GOAT (!!!)
     return (formatted_date == pd.Timestamp(date.today()) - timedelta(days=1))
 
-# Need to work on an interface, or if I'm just going to pass the abbreviation through the command line
 
-
-# Gets team data for the frame
-abbreviation = 'LAC'
-teamID = get_team_id_abbrev(abbreviation)
-teamGameLog = teamgamelog.TeamGameLog(team_id=teamID)
-latest_game = get_latest_game(teamID)
-latest_game_ID = latest_game['Game_ID']
-
-# If the team played yesterday, get the box score
-if did_play_yesterday(teamID):
-    boxScoreFrames = box_score.BoxScoreTraditionalV2(game_id=latest_game_ID)
-else:
-    print('No game yesterday')
-    exit()
-
-# Grabbing the first dataframe from the get_data_frames() function, which is the box score
-df = boxScoreFrames.get_data_frames()[0]
-
-teamdf = boxScoreFrames.get_data_frames()[1]
-
-print(teamdf)
-
-
-# Dropping NaN values (Coach's decisions to not play)
-df = df.dropna()
-
-# Dropping columns that aren't needed
-dropped_columns = ['COMMENT', 'MIN', 'NICKNAME', 'TEAM_CITY', 'START_POSITION', 'GAME_ID', 'PLAYER_ID', 'TEAM_ID']
-for column in dropped_columns:
-    df = df.drop([column], axis='columns')
-
-# Converting the floats to ints where they are needed
-cleaned_columns = ['PTS', 'PLUS_MINUS', 'PF', 'TO', 'BLK', 'STL', 'AST', 'REB', 'DREB', 'OREB', 'FTA', 'FTM', 'FG3A', 'FG3M', 'FGA', 'FGM']
-for column in cleaned_columns:
-    df[[column]] = df[[column]].apply(np.int64)
-
-# Finding max value in a given column
-def max_value(column):
-    return df[column].max()
-
-# Finding the index of the max value in a given column
-def max_value_index(column):
-    return df[column].idxmax()
-
-def player_attribute_leader(column):
+# Gets the player that was the attribute leader in a certain category
+def player_attribute_leader(column, df):
     returnString = ''
     columnMax = df[column].max()
     columnMaxIndex = df[column].idxmax()
@@ -88,21 +47,68 @@ def player_attribute_leader(column):
     return returnString
 
 
-df = df.loc[df['TEAM_ABBREVIATION'] == abbreviation]
-pointsLeader = player_attribute_leader('PTS')
-#print(pointsLeader)
-
-leader_attributes = ['PTS', 'AST', 'REB', 'STL', 'BLK', 'TO', 'FG3M']
-
-for attribute in leader_attributes: 
-    tempLeader = player_attribute_leader(attribute)
-    #print(tempLeader)
-
-#print(pointsMax)
-#print(pointsMaxIndex)
-
-# Delete rows that do not have the team abbreviation 
-
-print(df)
+# Throwing out columns that are not needed, as well as players that did not play (comes up as NaN)
+def clean_df(df):
+    df = df.dropna()
+    return df
 
 
+def drop_columns(dropped_columns, df):
+    for column in dropped_columns:
+        df = df.drop([column], axis='columns')
+    return df
+
+
+def clean_columns(cleaned_columns, df):
+    for column in cleaned_columns:
+        df[[column]] = df[[column]].apply(np.int64)
+    return df
+
+
+def get_latest_game_ID(teamID):
+    teamGameLog = teamgamelog.TeamGameLog(team_id=teamID)
+    latest_game = get_latest_game(teamID)
+    return latest_game['Game_ID']
+
+
+if __name__ == '__main__':
+
+    # Gets team data for the frame
+    abbreviation = 'LAC'
+
+    teamID = get_team_id_abbrev(abbreviation)
+    latest_game_ID = get_latest_game_ID(teamID)
+    
+
+    # If the team played yesterday, get the box score
+    if did_play_yesterday(teamID):
+        boxScoreFrames = box_score.BoxScoreTraditionalV2(game_id=latest_game_ID)
+    else:
+        print('No game yesterday')
+        exit()
+    
+    # Grabbing the first dataframe from the get_data_frames() function, which is the box score
+    playerBoxScore = boxScoreFrames.get_data_frames()[0]
+
+    teamBoxScore = boxScoreFrames.get_data_frames()[1]
+    print(teamBoxScore)
+
+    dropped_columns = ['COMMENT', 'MIN', 'NICKNAME', 'TEAM_CITY', 'START_POSITION', 'GAME_ID', 'PLAYER_ID', 'TEAM_ID', 'PLUS_MINUS']
+    playerBoxScore = drop_columns(dropped_columns, playerBoxScore)
+
+    # Converting the floats to ints where they are needed
+    cleaned_columns = ['PTS', 'PF', 'TO', 'BLK', 'STL', 'AST', 'REB', 'DREB', 'OREB', 'FTA', 'FTM', 'FG3A', 'FG3M', 'FGA', 'FGM']
+    playerBoxScore = clean_columns(cleaned_columns, playerBoxScore)
+
+    playerBoxScore = clean_df(playerBoxScore)
+
+    # Delete rows that do not have the team abbreviation 
+    playerBoxScore = playerBoxScore.loc[playerBoxScore['TEAM_ABBREVIATION'] == abbreviation]
+
+    leader_attributes = ['PTS', 'AST', 'REB', 'STL', 'BLK', 'TO', 'FG3M']
+
+    for attribute in leader_attributes: 
+        leader = player_attribute_leader(attribute, playerBoxScore)
+        print(leader)
+
+    print(playerBoxScore)
